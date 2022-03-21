e9c29db21383f8fe44cc930fab47eb33f9b8239f
Sergey Margelov <smargelov@gmail.com>
2022-03-21 22:15:35 +0300

    Add usd price in db

--- src/components/TheCatalog.vue
+++ src/components/TheCatalog.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { defineProps} from 'vue'
 import AppProductCard from '@/components/AppProductCard.vue'
 const props = defineProps({
     catalog: {
--- src/components/TheHelloText.vue
+++ src/components/TheHelloText.vue
@@ -1,7 +1,4 @@
 <script setup>
-import {defineProps} from 'vue'
-
-
 const props = defineProps({
     productCount: {
         type: Number,
